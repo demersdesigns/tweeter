d9ef2d7561457fad1a43b59ae8db215362dda04c
Paul Demers <paul@pdemersjr.com>
2011-12-28 19:57:17 -0500

    Revert "Revert e5f618f8203f9122ecb37face32e3ebdb97215e3^..HEAD"
    This reverts commit 82bc7db64f5e27c283b19c4b5165dec9b67f5f11.

--- js/model.js
+++ js/model.js
@@ -1,27 +1,63 @@
-var userList = [
-{name: 'John Cleese', twitterName: 'JohnCleese'},
-{name: 'MC Hammer', twitterName: 'MCHammer'},
-{name: 'Stephen Fry', twitterName: 'StephenFry'},
-{name: 'Al Gore', twitterName: 'algore'},
-{name: 'Steven Sanderson', twitterName: 'StevenSanderson'}
-];
-
 var viewModel = {
   appName: 'Tweeted',
   
   //logged in user bindings
-  username: ko.observable(""),
-  propername: ko.observable(""),
-  avatar: ko.observable(""),
-  joinedDate: ko.observable(""),
-  tweetCount: ko.observable(""),
-  followerCount: ko.observable(""),
-  followingCount: ko.observable(""),
-  latestTweets: ko.observableArray([]),
+  name: ko.observable(""),
+  screen_name: ko.observable(""),
+  friends_count: ko.observable(""),
+  followers_count: ko.observable(""),
+  statuses_count: ko.observable(""),
+  profile_image_url: ko.observable(""),
   
   //follower user bindings
-  userList: ko.observableArray(userList),
-  selectedUser: ko.observable([]),
-  userLatestTweets: ko.observableArray([])
-};  
+  userList: ko.observableArray([])
+}
 ko.applyBindings(viewModel);
+
+//Return the 20 latest friends and populate the userList
+getUserList = function(){
+  $.getJSON('https://api.twitter.com/1/statuses/friends.json?screen_name=demersdesigns&count=20&callback=?', function(data){
+    
+    //Get latest data first
+    var data = data.reverse();
+    
+    for(i=0, max=data.length; i<max; i++){
+      //Shortcut so we don't have to write data[i] every time
+      var content = data[i];
+      
+      //Clear out any existing data
+      viewModel.userList([]);
+      
+      //Push data into array
+      viewModel.userList.push({
+        name: content.name,
+        screen_name: content.screen_name,
+        friends_count: content.friends_count,
+        followers_count: content.followers_count,
+        statuses_count: content.statuses_count,
+        profile_image_url: content.profile_image_url
+      });
+    }
+  });
+}
+
+//Return logged in user's information
+//Since there is not a login/logout system in this demo, we hardcode the logged in user
+getUserInfo = function(){
+  $.getJSON('https://api.twitter.com/1/users/lookup.json?screen_name=demersdesigns&callback=?', function(data){
+
+      var content = data[0];
+      
+      //Push loggeed in user data into model
+        viewModel.name(content.name),
+        viewModel.screen_name(content.screen_name),
+        viewModel.friends_count(content.friends_count),
+        viewModel.followers_count(content.followers_count),
+        viewModel.statuses_count(content.statuses_count),
+        viewModel.profile_image_url(content.profile_image_url)
+  });
+}
+
+//Call the initial functions to get data into the model
+getUserInfo();
+getUserList();
